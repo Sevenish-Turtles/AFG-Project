5e01b201252dc2d7b66fcec6b8ee22363f3f1aef
Jackie <janderson@wpi.edu>
2020-04-24 13:43:52 -0400

    edit template

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,11 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.widget.Button;
+import android.content.Intent;
+import android.view.View;
+
 public class MainActivity extends AppCompatActivity {
+public Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.EditButton);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, NewQuestion.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
 }
