928e9f52b4db00c44bfcda8a3b7eb2cb55244e84
anika <asaxena@wpi.edu>
2020-05-04 14:17:51 -0400

    Edit Question Template

--- app/src/main/java/com/example/myapplication/QuestionPractice.java
+++ app/src/main/java/com/example/myapplication/QuestionPractice.java
@@ -4,11 +4,50 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.widget.Button;
+import android.content.Intent;
+import android.view.View;
+
+
 public class QuestionPractice extends AppCompatActivity {
+    public Button backButtonQuestionPractice;
+    public Button solutionButton;
+    public Button editQuestionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question_practice);
+
+        Button backButtonQuestionPractice = (Button)findViewById(R.id.BackButton);
+
+        backButtonQuestionPractice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(QuestionPractice.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button solutionButton = (Button)findViewById(R.id.SolutionButton);
+
+        solutionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(QuestionPractice.this, Solution.class);
+                startActivity(intent);
+            }
+        });
+
+        Button editQuestionButton = (Button)findViewById(R.id.EditQuestionButton);
+
+        editQuestionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(QuestionPractice.this, EditQuestion.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
