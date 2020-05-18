ef70836d898b420dafa15ff01c477c306d4a00e0
Toyesh_Jayaswal <toyesh120@gmail.com>
2020-05-18 19:08:35 -0400

    bug fixes and small edits

--- app/src/main/java/com/example/myapplication/QuestionPractice.java
+++ app/src/main/java/com/example/myapplication/QuestionPractice.java
@@ -88,6 +88,7 @@ public class QuestionPractice extends AppCompatActivity {
         final QuestionDatabase db = QuestionDatabase.getInstance(this);
         questions = db.questionDao().getQuestionsForDeck(deck);
         askQuestion();
+        //
     }
 
     public void askQuestion() {
